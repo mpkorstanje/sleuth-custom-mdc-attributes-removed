c10b179418186cea2efa046c463805888e1b15d2
M.P. Korstanje <logarithmicwhale@gmail.com>
2019-08-27 13:16:42 +0200

    Use spring-cloud Hoxton.M2

--- src/main/java/com/example/demo/controller/Configuration.java
+++ src/main/java/com/example/demo/controller/Configuration.java
@@ -3,7 +3,6 @@ package com.example.demo.controller;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Component
 @ConfigurationProperties("com.example")
 public class Configuration {
 
--- src/main/java/com/example/demo/controller/GreetingController.java
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -1,16 +1,16 @@
 package com.example.demo.controller;
 
 import org.slf4j.MDC;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Objects;
-
 import static java.util.Objects.requireNonNull;
 
 @RestController
+@EnableConfigurationProperties(Configuration.class)
 public class GreetingController {
 
     private final RestTemplate restTemplate;
